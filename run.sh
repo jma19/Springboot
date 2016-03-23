201669ae8144c5ebba389c2277b86c3b2b86b8fc
majun <jun.mash@ele.me>
2016-03-23 23:56:45 +0800

    push all

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 mvn clean install -DskipTests=true
 cd target
 mv RegisterService-0.0.1-SNAPSHOT.war RegisterService.war
-scp RegisterService.war root@120.25.79.100:/home/apache-tomcat-8.0.32/webapps
+scp RegisterService.war XXX@XXX:/home/apache-tomcat-8.0.32/webapps
